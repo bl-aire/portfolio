8d5e254e7045f70efed251c680b3a6cd3cca5a04
Blaire <ogoh_blessing@yahoo.com>
2022-07-08 11:41:05 +0100

    Create Data Service

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contact';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  postContactForm(Contact: Contact) {
+    
+  }
+}
